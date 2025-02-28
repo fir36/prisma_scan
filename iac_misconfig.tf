c4a8cfec4503f5de81632c1393ebf0225d81ca88
fir36 <110829845+fir36@users.noreply.github.com>
2025-02-28 16:06:13 +0800

    Update iac_misconfig.tf

--- iac_misconfig.tf
+++ iac_misconfig.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 } 
-
+  
 # 🚨 Insecure S3 Bucket: Public Access Enabled 
 resource "aws_s3_bucket" "insecure_bucket" {
   bucket = "prisma-cloud-insecure-bucket"
